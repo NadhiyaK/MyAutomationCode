edee5653fd8544413a9ea733172c278b2d72c801
Karunakaran <nadhiyakarunakaran28@gmail.com>
2022-02-27 20:58:39 -0500

    final push

---
+++ Auto/src/test/java/Auto/Auto/Gittest.java
@@ -0,0 +1,14 @@
+package Auto.Auto;
+
+import org.testng.annotations.Test;
+
+public class Gittest {
+
+	@Test
+	public static void gittest()
+	{
+		System.out.println("This is for testing");
+		
+	}
+	
+}
